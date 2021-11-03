4f899d39ed159245364b728ef77a3d9a20e790a7
blck f0x <lytvyn.pavlo@gmail.com>
2021-11-03 20:14:12 +0200

    Добавьте файлы проекта.

---
+++ 5.1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double s, const double t); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = pow(h(t * t, 1), 3) + h(1, t * s * s);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b) // визначення
+{
+	return (pow(a, 2) * sin(b) + pow(b, 2) * sin(a));
+}
